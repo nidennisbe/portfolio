1d225b6a60d90ce38f638fdefbcbb1136b306013
nidennis be <nidennisbe@gmail.com>
2017-08-07 09:48:44 +0000

    testing3

--- bestultrabook.php
+++ bestultrabook.php
@@ -1,11 +1,17 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
 $server = $url["us-cdbr-iron-east-05.cleardb.net"];
 $username = $url["be9d4142c17943"];
 $password = $url["9986cb1b"];
-$db = $url["heroku_333cefb0b246c8d"];
+$db = substr($url["heroku_333cefb0b246c8d"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
+if ($conn->connect_error) {
+    //If failed to connect
+    die("Connection failed: " . $conn->connect_error);
+}
+
 
 	$result = mysqli_query($conn, "SELECT * FROM tb_page_ultrabook");
 ?>
