d20196851c9d964cb3353893257d25c97e9f8b13
nidennis be <nidennisbe@gmail.com>
2017-08-07 11:02:19 +0000

    Pushed from Dropbox

--- bestultrabook.php
+++ bestultrabook.php
@@ -6,13 +6,14 @@ $username = $url["be9d4142c17943"];
 $password = $url["9986cb1b"];
 $db = substr($url["heroku_333cefb0b246c8d"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
-
+$conn = mysqli_connect($server, $username, $password, $db);
+	
+$result = mysqli_query($conn, "SELECT * FROM tb_page_ultrabook");
 if ($conn->connect_error) {
     //If failed to connect
     die("Connection failed: " . $conn->connect_error);
 }
-	$result = mysqli_query($conn, "SELECT * FROM tb_page_ultrabook");
+	
 	
 	
 	
