2af320d05b09140bc41543ad20a326ea7aa49692
nidennis be <nidennisbe@gmail.com>
2017-08-07 11:26:39 +0000

    Pushed from Dropbox

--- bestultrabook.php
+++ bestultrabook.php
@@ -1,9 +1,12 @@
 <?php
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["us-cdbr-iron-east-05.cleardb.net"];
+$username = $url["be9d4142c17943"];
+$password = $url["9986cb1b"];
+$db = substr($url["heroku_333cefb0b246c8d"], 1);
 
-
-$conn = mysqli_connect($url);
+$conn = mysqli_connect($server, $username, $password, $db);
 	
 
 if ($conn->connect_error) {
