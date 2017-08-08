430cfdf339f0534193f002a7d4d629c29579308b
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-08 18:33:49 +1200

    update header

--- header.php
+++ header.php
@@ -3,33 +3,22 @@
 <head>
     <link rel="stylesheet" type="text/css" href="style/mobile_version.css"/>
     <link rel="stylesheet" type="text/css" href="style/text_effect.css"/>
-
-    <meta property="og:url"           content="http://www.your-domain.com/your-page.html" />
-  <meta property="og:type"          content="website" />
-  <meta property="og:title"         content="IO TECH REVIEW" />
-  <meta property="og:description"   content="Latest tech review" />
-  <meta property="og:image"         content="http://www.your-domain.com/path/image.jpg" />
 </head>
 <header>
+  <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <center>
     <div class="header_wrapper">
     <div class="clip-text clip-text_tree">IO TECH REVIEW</div>
     </div>
 
-    <div class="fb-like"
-  data-href="http://www.your-domain.com/your-page.html"
-  data-layout="standard"
-  data-action="like"
-  data-show-faces="true">
-</div>
-<div id="fb-root"></div>
-  <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="https://www.facebook/nidennisbe" data-width="50px" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="false"></div>
 
 </header>
 </html>
