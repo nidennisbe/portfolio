c1c3e0a47717563885f7e83eed38de8b93cb72a9
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-08 17:56:25 +1200

    updated removing substring from database name

--- nav.php
+++ nav.php
@@ -8,7 +8,7 @@
             <a class="element_nav1" href="bestultrabook.php">Ultrabooks</a>
             <a class="element_nav2" href="bestgaminglaptop.php">Gaming Laptops</a>
             <a class="element_nav3" href="aboutus.php">About Us</a>
-            <a class="btn_sign_up"  href="sign_in.php">NY Account</a>
+            <a class="btn_sign_up"  href="sign_in.php">My Account</a>
             <br />
       </a>
     </div>
