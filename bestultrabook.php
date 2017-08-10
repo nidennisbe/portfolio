0e934bc89030f33e9fc561bd5f334ec34e4ed053
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-10 19:19:52 +1200

    time

--- bestultrabook.php
+++ bestultrabook.php
@@ -40,6 +40,8 @@ if ($conn->connect_error) {
 								while ($row = mysqli_fetch_array($result)) {
 									echo "<div id='img_div'><a href='".$row['url']."' </a>";
 									echo "<img src='".$row['image']."' >";
+                  echo "<p id='p-desc-item'>".$row['date']."<p>";
+                  echo "</div>";
 							    echo "<h3 id='h3-title-item'>".$row['title']."</h3>";
 							    echo "<div >";
 									echo "<p id='p-desc-item'>".$row['description']."<p>";
