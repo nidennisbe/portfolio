7644be3ccc9f8013bda211324b702533d7f7a864
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-11 17:19:49 +1200

    a

--- review-detail.php
+++ review-detail.php
@@ -37,7 +37,7 @@ include "nav.php";
 <center>
 
 ?>
-<div name="title" style="border-bottom: solid 1px #6998ba;height:100px;"><h1 style="color:#6998ba;padding:35px" ><strong>  <h3 id="h3-title-item"><?php echo $row['title'];?></h3></strong></h1></div>
+<div name="title" style="border-bottom: solid 1px #6998ba;height:100px;"><h3 style="color:#6998ba;padding:35px" >  <h3 id="h3-title-item"><?php echo $row['title'];?></h3></div>
 <section
 <div id="content">
   <?php do { ?>
