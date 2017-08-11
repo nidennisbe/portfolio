22c48183afc0f300cba7e9f4c0adaf70ee0c5f1a
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-11 18:13:29 +1200

    a

--- bestultrabook.php
+++ bestultrabook.php
@@ -44,7 +44,7 @@ if ($conn->connect_error) {
     							    <h3 id="h3-title-item"><?php echo $row['title'];?></h3>
     									<p id="p-desc-item"><?php echo $row['description'];?><p>
 						      </div>
-          <?php } while ($row = mysqli_fetch_array($result)) ?>
+          <?php } while ($row = mysqli_fetch_assoc($result)) ?>
         </section>
 <?php
 include "footer.php";
