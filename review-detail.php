ed0902dbe52af11a83b2c0725615ddc5e2ac291c
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-11 17:27:31 +1200

    a

--- review-detail.php
+++ review-detail.php
@@ -37,12 +37,12 @@ include "nav.php";
 <center>
 
 ?>
-  <?php do { ?>
-<div name="title" style="border-bottom: solid 1px #6998ba;height:100px;"><h1 style="color:#6998ba;padding:35px" >  <h1><?php echo $row['title'];?></h1></div>
+<div name="title" style="border-bottom: solid 1px #6998ba;height:100px;"><h1 style="color:#6998ba;padding:35px" ><strong><?php echo $result['title'];?></strong></h1></div>
 <section
 <div id="content">
-
+  <?php do { ?>
         <img src="<?php echo $row['image'];?>" >
+        <h3 id="h3-title-item"><?php echo $row['title'];?></h3>
         <p id="p-desc-item"><?php echo $row['description'];?><p>
         </div>
           <?php } while ($row = mysqli_fetch_array($result)) ?>
