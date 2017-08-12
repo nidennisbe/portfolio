1ed41bce067ed59320d7453f02689281bbe653ee
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-12 21:36:30 +1200

    a

--- trending_section.php
+++ trending_section.php
@@ -25,27 +25,25 @@
   <div name="title" style="background-color:#89b724;height:100px;"><h1 style="color:white;padding:35px" ><strong>Top Ultrabooks</strong></h1></div>
 	<section>
 				<div id="content">
+
 							<?php do { ?>
-
   <div class="wrapper">
-    <div class="product-img">
-      <img src="<?php echo $row['image'];?>" height="420" width="100%">
-    </div>
-    <div class="product-info">
-      <div class="product-text">
-        <h1><?php echo $row['title'];?></h1>
-        <h2>latest modified: <?php echo $row['date'];?></h2>
-        <p><?php echo $row['description'];?> </p>
-      </div>
-    </div>
-		<div class="product-price-btn">
-			<button type="button">Read more</button>
-		</div>
+				    <div class="product-img">
+				      <img src="<?php echo $row['image'];?>" height="420" width="100%">
+				    </div>
+				    <div class="product-info">
+				      <div class="product-text">
+				        <h1><?php echo $row['title'];?></h1>
+				        <h2>latest modified: <?php echo $row['date'];?></h2>
+				        <p><?php echo $row['description'];?> </p>
+				      </div>
+				    </div>
+						<div class="product-price-btn">
+							<button type="button">Read more</button>
+						</div>
   </div>
-
-<?php }
+					<?php }
 					while ($result && $row = mysqli_fetch_assoc($result))
-
 					 ?>
 
         </section>
