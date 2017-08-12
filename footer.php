42217011d5af2b24a8256ceaf41b2b55e060b59d
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-12 15:50:18 +1200

    a

--- footer.php
+++ footer.php
@@ -9,16 +9,16 @@
             <a class="element_footer" href="bestgaminglaptop.php">Gaming Laptops</a>
             <a class="element_footer" href="aboutus.php">Contact Us</a>
 
-          <ul class="social-icons"style="background:black;bottom:0">
+          <ul class="social-icons"style="background:black;">
                  <h1 style=" font-family: 'impact', sans-serif;">FOLLOW US</h1>
                  <li><a href="http://www.facebook.com" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
                  <li><a href="http://www.plus.google.com" class="social-icon"> <i class="fa fa-google"></i></a></li>
                  <li><a href="http://www.twitter.com" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
                  <li><a href="http://www.youtube.com" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
                  <li><a href="http://www.instagram.com" class="social-icon"> <i class="fa fa-instagram"></i></a></li>
-                 <h5 style=" font-family: sans-serif;font-size:20px;color:white">&copy Copyright 2017 | IO Tech Review | All rights reserved</h5>
-           </ul>
 
+           </ul>
+                <h5 style=" font-family: sans-serif;font-size:20px;color:white;background:black">&copy Copyright 2017 | IO Tech Review | All rights reserved</h5>
         </div>
     </div>
 </footer>
