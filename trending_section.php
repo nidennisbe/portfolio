329f378e97497a4cb5d71a4e87547464fa7c98f7
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-12 20:17:22 +1200

    a

--- trending_section.php
+++ trending_section.php
@@ -1,13 +1,21 @@
-
 <?php
-	$db = mysqli_connect("localhost", "root", "Benidennis9", "webdb");
-	$result = mysqli_query($db, "SELECT * FROM tb_top_stories");
-?>
+        $server = "h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $username = "t8mvn41zc3dvlxhu";
+        $password = "fhzr9pbe4y18yxlz";
+        $db = "k6zc9jmzecfdzsgk";
+        $conn = mysqli_connect($server, $username, $password, $db);
 
+        if ($conn->connect_error) {
+            //If failed to connect
+            die("Connection failed: " . $conn->connect_error);
+        }
+        	$result = mysqli_query($conn, "SELECT * FROM tb_page_gaminglaptop");
+?>
 <!DOCTYPE html>
 <html>
 <head>
 			<link rel="stylesheet" type="text/css" href="style/card_layout.css"/>
+			<link rel="stylesheet" type="text/css" href="style/card_layout-grid.css"/>
       <link rel="stylesheet" type="text/css" href="style/stylesheet.css"/>
       <link rel="stylesheet" type="text/css" href="style/text_effect.css"/>
       <link rel="stylesheet" type="text/css" href="style/mobile_version.css"/>
@@ -23,17 +31,28 @@
   <div name="title" style="background-color:#89b724;height:100px;"><h1 style="color:white;padding:35px" ><strong>Top Ultrabooks</strong></h1></div>
 	<section>
 				<div id="content">
-							<?php
-								while ($row = mysqli_fetch_array($result)) {
-									echo "<div id='img_div'><a href='".$row['url']."' </a>";
-									echo "<img src='".$row['image']."' >";
-							    echo "<h3>".$row['title']."</h3>";
-							    echo "<div class='desc'>";
-									echo "<p>".$row['description']."<p>";
-								echo "</div>";
-						echo "</div>";
-								}
-							?>
+							<?php do { ?>
+								<body>
+  <div class="wrapper">
+    <div class="product-img">
+      <img src="<?php echo $row['image'];?>" height="420" width="327">
+    </div>
+    <div class="product-info">
+      <div class="product-text">
+        <h1><?php echo $row['title'];?></h1>
+        <h2>latest modified: <?php echo $row['date'];?></h2>
+        <p><?php echo $row['description'];?> </p>
+      </div>
+      <div class="product-price-btn">
+        <p><span>78</span>$</p>
+        <button type="button">Read more</button>
+      </div>
+    </div>
+  </div>
+
+</body>
+          <?php } while ($row = mysqli_fetch_assoc($result)) ?>
+        </section>
 <?php
 include "footer.php";
 ?>
