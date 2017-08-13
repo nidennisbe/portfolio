7efba152a716421eb00fe4f2d6cf7029a2b620f1
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-13 18:59:41 +1200

    a

--- index.php
+++ index.php
@@ -42,13 +42,12 @@
         <div class="container">
                    <h1 class="title_goal">Our missions</h1>
                    <div class="box">
-                   <a href="bestultrabook.php"><img src="./pics/goal/laptop_image.jpg"style="width:60%"></a>
+                   <a href="bestultrabook.php"><img src="./pics/goal/laptop_image.jpg"style="width:550px;height:auto"></a>
                    <h3 class="text-goal-h3">Laptops review</h3>
                    <p>Offer best articles about tech base on our experiences of using those devices</p>
              </div>
-
              <div class="box">
-                   <a href="bestgaminglaptop.php"><img src="./pics/goal/update.jpg"style="width:49%"></a>
+                   <a href="bestgaminglaptop.php"><img src="./pics/goal/update.jpg"style="width:550px;height:auto"></a>
                    <h3>Latest Review</h3>
                    <p>Deliver the up-todate technology tending </p>
              </div>
