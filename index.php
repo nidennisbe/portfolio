adcea38fed386ad5f17bed3a7d9041406590c514
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-13 22:23:59 +1200

    ab

--- index.php
+++ index.php
@@ -26,8 +26,7 @@
       </div>
     </div>
 <center>
-<section class="slideshow-home" style="background-color:#8bc1e9;height:auto;overflow:auto;
-        clear: both;">
+<section class="slideshow-home" style="background-color:#8bc1e9;">
   <div class="w3-content w3-display-container" style="margin:0 auto;">
         <img class="mySlides" src="pics/slideshow/razerblade.png" style="width:550px; height: auto;">
         <img class="mySlides" src="pics/slideshow/dellxps.png" style="width:550px; height: auto;">
