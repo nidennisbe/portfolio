889de5c3a966412250bbe1d48910da5e1ba22031
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-13 13:13:16 +1200

    a

--- trending_section.php
+++ trending_section.php
@@ -23,7 +23,7 @@
 <body>
 
 <section>
-		  <div name="title" style="background-color:#89b724;height:100px;"><h1 style="color:white;padding:35px" ><strong>Trending</strong></h1></div>
+		  <div name="title" style="background-color:#8BC1E9;height:100px;"><h1 style="color:white;padding:35px" ><strong>Trending</strong></h1></div>
 				<div id="content">
 <?php while ($result && $row = mysqli_fetch_assoc($result)) {?>
 	<div class="wrapper">
