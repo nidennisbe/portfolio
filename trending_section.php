791c8993c5d519691c1f86b8d2b6b6ae8726d23b
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-13 12:46:54 +1200

    a

--- trending_section.php
+++ trending_section.php
@@ -25,7 +25,7 @@
 <section>
 		  <div name="title" style="background-color:#89b724;height:100px;"><h1 style="color:white;padding:35px" ><strong>Trending</strong></h1></div>
 				<div id="content">
-<?php do { ?>
+<?php while ($result && $row = mysqli_fetch_assoc($result))
 	<div class="wrapper">
 				    	<div class="product-img">
 				      		<img src="<?php echo $row['image'];?>" height="420" width="100%">
@@ -42,8 +42,8 @@
 				       </div>
   </div>
 <?php }
-				while ($result && $row = mysqli_fetch_assoc($result))
-?>
+				?>
+
 
 </section>
 </div>
