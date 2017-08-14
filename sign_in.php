c136de7fa41c5dcb40f2b25b86724023e3878ef9
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-15 09:48:46 +1200

    relocate signinup css

--- sign_in.php
+++ sign_in.php
@@ -7,9 +7,9 @@
     <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
     <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
     <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-    <link rel="stylesheet" href="style/stylesheet.css">
-    <link rel="stylesheet" href="style/signupstyle.css">
-    <link rel="stylesheet" href="style/mobile_version.css">
+    <link rel="stylesheet" href="resources/css/stylesheet.css">
+    <link rel="stylesheet" href="resources/css/signupstyle.css">
+    <link rel="stylesheet" href="resources/css/mobile_version.css">
     <meta name="viewport" content="width=680px">
 </head>
 <!-- ---------------------------START BODY------------------------------------------------- -->
