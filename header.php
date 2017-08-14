6581f816151fc399a4d04865b4fcc4e5613b6ed8
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-15 09:46:05 +1200

    nav height update

--- header.php
+++ header.php
@@ -1,18 +1,11 @@
 
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="style/mobile_version.css"/>
-    <link rel="stylesheet" type="text/css" href="style/text_effect.css"/>
+    <link rel="stylesheet" type="text/css" href="resources/css/mobile_version.css"/>
+    <link rel="stylesheet" type="text/css" href="resources/css/text_effect.css"/>
 </head>
 <header>
-  <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+
 <center>
     <div class="header_wrapper">
     <div class="clip-text clip-text_tree">IO TECH REVIEW</div>
