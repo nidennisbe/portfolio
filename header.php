f68c97c4d04a0fbbeb804dccfad2960c0fe0f1ff
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-15 16:48:46 +1200

    facebook share penal

--- header.php
+++ header.php
@@ -11,7 +11,5 @@
     <div class="clip-text clip-text_tree">IO TECH REVIEW</div>
     </div>
 
-    <div class="fb-like " style="  position: absolute;  top: 0; right: 0;" data-href="https://www.facebook.com/NDBLivepiano/" data-width="50px" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="false"></div>
-
 </header>
 </html>
