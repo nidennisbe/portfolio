9a2c673fb6083fbbead0d1a6e396c0654407990b
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-15 22:02:23 +1200

    facebook share penal

--- ultrabook.php
+++ ultrabook.php
@@ -1,19 +1,9 @@
-<?php
-        $server = "h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        $username = "t8mvn41zc3dvlxhu";
-        $password = "fhzr9pbe4y18yxlz";
-        $db = "k6zc9jmzecfdzsgk";
-        $conn = mysqli_connect($server, $username, $password, $db);
 
-        if ($conn->connect_error) {
-            //If failed to connect
-            die("Connection failed: " . $conn->connect_error);
-        }
-        	$result = mysqli_query($conn, "SELECT * FROM tb_page_ultrabook");
-?>
 <!doctype html>
 <html>
 <head>
+      <?php include "resources/dbconnector/mysql-connector.php";?>
+      <?php $result = mysqli_query($conn, "SELECT * FROM tb_page_ultrabook");?>
       <link rel="icon" href="pics/nidennis.jpg">
       <meta charset="utf-8">
       <title>Ultrabooks</title>
