3db28c79cefa2e90ee6cfe797863cc33406dc83b
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-15 17:02:12 +1200

    facebook share penal

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
                  <li><a href="http://www.instagram.com" class="social-icon"> <i class="fa fa-instagram"></i></a></li>
                 <h5 style=" font-family: sans-serif;font-size:20px;color:white">&copy Copyright 2017 | IO Tech Review | All rights reserved</h5>
            </ul>
-           <div class="fb-page" style="float:right;top:0; position: absolute;top: 100;right: 0;padding-right:48px"data-href="https://www.facebook.com/NDBLivepiano/" data-tabs="timeline" data-height="100" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/NDBLivepiano/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/NDBLivepiano/">IO TECH</a></blockquote></div>
+           <div class="fb-page" style="float:right;top:0; position: absolute;top: 200px;right: 0;padding-right:48px"data-href="https://www.facebook.com/NDBLivepiano/" data-tabs="timeline" data-height="100" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/NDBLivepiano/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/NDBLivepiano/">IO TECH</a></blockquote></div>
         </div>
     </div>
 </footer>
