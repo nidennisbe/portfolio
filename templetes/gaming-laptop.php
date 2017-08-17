69a4295a5e2946b719ac71eb8c34effbf26d2d2c
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-17 15:10:44 +1200

    relocate file to templetes

--- templetes/gaming-laptop.php
+++ templetes/gaming-laptop.php
@@ -16,8 +16,8 @@
 </head>
 <body>
 <?php
-      include "templetes/header.php";
-      include "templetes/nav.php";
+      include "../templetes/header.php";
+      include "../templetes/nav.php";
 ?>
 <div name="title" style="border-bottom: solid 1px #6998ba;height:100px"><h1 style="color:#6998ba;padding:35px" ><strong>GAMING LAPTOPS REVIEW</strong></h1></div>
 <section>
@@ -41,7 +41,7 @@
         				?>
 </section>
 <?php
-      include "footer.php";
+      include "../templetes/footer.php";
 ?>
 </body>
 </html>
