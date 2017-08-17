1b86064640a8e0e673304985c0b63a8cd4d8b175
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-17 15:20:19 +1200

    relocate file to templetes

--- templetes/nav.php
+++ templetes/nav.php
@@ -5,10 +5,10 @@
     <div class="topnav" id="wrapper_nav">
       <a href="javascript:void(0);" style="font-size:26px;" class="icon" onclick="myFunction()">&#9776;</a>
             <a class="element_nav"  href="index.php">Home</a>
-            <a class="element_nav1" href="ultrabook.php">Ultrabooks</a>
-            <a class="element_nav2" href="gaming-laptop.php">Gaming Laptops</a>
-            <a class="element_nav3" href="about.php">About</a>
-            <a class="btn_sign_up"  href="sign_in.php">My Account</a>
+            <a class="element_nav1" href="../templetes/ultrabook.php">Ultrabooks</a>
+            <a class="element_nav2" href="../templetes/gaming-laptop.php">Gaming Laptops</a>
+            <a class="element_nav3" href="../templetes/about.php">About</a>
+            <a class="btn_sign_up"  href="../templetes/sign_in.php">My Account</a>
       </a>
     </div>
 </nav>
