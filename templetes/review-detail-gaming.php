2095435bd3ca21e4b796c104c1e98e37e77394a2
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-08-21 21:37:58 +1200

    mission box

--- templetes/review-detail-gaming.php
+++ templetes/review-detail-gaming.php
@@ -39,7 +39,7 @@
                     <p id="p-desc-item"><?php echo $row['description'];?><p>
                     </div>
               <?php }  ?>
-              <div class="fb-comments"style="float:left;margin-left:10%;margin-top:5%;margin-bottom:3% box-shadow: 0px 14px 32px 0px rgba(0, 0, 0, 0.15);" data-href="http://iotech.herokuapp.com/review-detail-gaming.php?id=<?php echo $id?>" data-numposts="4"></div>
+              <div class="fb-comments"style="font-weight:bold;font-size:1.2em;float:left;margin-left:10%;margin-top:5%;margin-bottom:3% box-shadow: 0px 14px 32px 0px rgba(0, 0, 0, 0.15);" data-href="http://iotech.herokuapp.com/review-detail-gaming.php?id=<?php echo $id?>" data-numposts="4"></div>
 </section>
 </section>
 <?php
