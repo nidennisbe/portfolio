0d323415b976d112d15cb15138603085ba9dd030
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2017-09-08 00:16:23 +1200

    bg footer

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
       </div>
     </div>
 <center>
-<section class="slideshow-home" style="background-color:#8bc1e9;">
+<section class="slideshow-home" style="background-color:#E95589;">
   <center>
   <div class="w3-content w3-display-container" style="margin:0 auto;">
         <img class="mySlides" src="resources/pics/slideshow/razerblade.png" style="width:550px; height: auto;">
