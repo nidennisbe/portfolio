8fc0665400a19dcbd13937ac15f6ed16be67ff07
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2018-05-11 10:57:07 +1200

    update index

--- index.php
+++ index.php
@@ -20,13 +20,11 @@
                    <h1 class="title_goal">Personal Details</h1>
                    <div class="box">
                          <a href="templetes/ultrabook.php"><img class="profile-image" src="./resources/pics/nidennis.jpg"style="width:250px;height:auto"></a>
-                         <h3 class="text-goal-h3">Summary</h3>
-                         <p>I have a few years of experience in building native Android, desktop and Web application. Beside that I really love designing UI for mobile applications. I enjoy working with technologies as have a strong appreciation for its ability to provide solutions to people and businesses. I can’t wait to learn the new technologies that come out everyday.</p>
+
                    </div>
                    <div class="box">
-                         <a href="templetes/gaminglaptop.php"><img src="./resources/pics/goal/update.jpg"style="width:500px;height:auto"></a>
-                         <h3>Latest Review</h3>
-                         <p>Deliver up-to-date technology and accurate information </p>
+                     <h3 class="text-goal-h3">Summary</h3>
+                     <p>I have a few years of experience in building native Android, desktop and Web application. Beside that I really love designing UI for mobile applications. I enjoy working with technologies as have a strong appreciation for its ability to provide solutions to people and businesses. I can’t wait to learn the new technologies that come out everyday.</p>
                    </div>
            </div>
 </section>
