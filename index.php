661cbedb1270e2edad12305396fe1b8f5ec50fa7
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2018-05-11 11:31:13 +1200

    upate colors

--- index.php
+++ index.php
@@ -14,13 +14,20 @@
       include "templetes/nav.php";
 ?>
 
-
 <section id="boxes">
             <div class="container">
                    <h1 class="title_goal">Personal Details</h1>
                    <div class="box">
                          <a href="templetes/ultrabook.php"><img class="profile-image" src="./resources/pics/nidennis.jpg"style="width:250px;height:auto"></a>
-
+                         <ul class="social-icons">
+                                <h1 style=" font-family: 'impact', sans-serif;">FOLLOW US</h1>
+                                <li><a href="http://www.facebook.com" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
+                                <li><a href="http://www.plus.google.com" class="social-icon"> <i class="fa fa-google"></i></a></li>
+                                <li><a href="http://www.twitter.com" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
+                                <li><a href="http://www.youtube.com" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
+                                <li><a href="http://www.instagram.com" class="social-icon"> <i class="fa fa-instagram"></i></a></li>
+                               <h5 style=" font-family: sans-serif;font-size:20px;color:white">&copy Copyright 2017 | IO Tech Review | All rights reserved</h5>
+                          </ul>
                    </div>
                    <div class="box">
                      <h3 class="text-goal-h3">Summary</h3>
