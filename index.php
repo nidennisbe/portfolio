cc3f6cb7e05a4090ed7b917463191781177cdbce
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2018-05-11 12:01:04 +1200

    upate colors

--- index.php
+++ index.php
@@ -16,17 +16,19 @@
 
 <section id="boxes">
             <div class="container">
-                   <h1 class="title_goal">Personal Details</h1>
+
                    <div class="box">
+                      <h1 class="title_goal">NIDENNIS BE</h1>
                          <a href="templetes/ultrabook.php"><img class="profile-image" src="./resources/pics/nidennis.jpg"style="width:250px;height:auto"></a>
+                           <h3 class="title_goal">Mobile Developer | UI designer</h3>
                          <ul class="social-icons">
-                                <h1 style=" font-family: 'impact', sans-serif;">FOLLOW US</h1>
+
                                 <li><a href="http://www.facebook.com" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
                                 <li><a href="http://www.plus.google.com" class="social-icon"> <i class="fa fa-google"></i></a></li>
                                 <li><a href="http://www.twitter.com" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
                                 <li><a href="http://www.youtube.com" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
                                 <li><a href="http://www.instagram.com" class="social-icon"> <i class="fa fa-instagram"></i></a></li>
-                               <h5 style=" font-family: sans-serif;font-size:20px;color:white">&copy Copyright 2017 | IO Tech Review | All rights reserved</h5>
+
                           </ul>
                    </div>
                    <div class="box">
@@ -37,6 +39,7 @@
 </section>
 <?php
       include "templetes/trending_section.php";
+        include "templetes/trending_section.php";
 ?>
 
 <?php
