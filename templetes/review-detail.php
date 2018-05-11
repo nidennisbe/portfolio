8b727cf7a1e863df08e670f662aedd0ccef7b4c7
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2018-05-11 17:04:27 +1200

    update information

--- templetes/review-detail.php
+++ templetes/review-detail.php
@@ -31,14 +31,14 @@
 ?>
 
 <center>
-<div name="title" style="border-bottom: solid 1px #c8213a;height:100px;float:left;"><h1 style="color:#FF5077;padding:35px" ><strong>Detail</strong></h1></div>
+<div name="title" style="border-bottom: solid 1px #c8213a;height:100px;float:left;"><h1 style="color:#cf1631;padding:35px" ><strong>Detail</strong></h1></div>
 <section>
             <div id="content">
               <?php while ($row = mysqli_fetch_assoc($result))  { ?>
                     <img class="img-detail" src="<?php echo $row['imageUrl'];?>" >
                     <h3 id="h3-title-item"><?php echo $row['title'];?></h3> <p>PLATFORM: <?php echo $row['platform'];?></p>
                     <hr>
-                    <p id="p-desc-item"><?php echo $row['description'];?><p>
+                    <h2 id="p-desc-item"><?php echo $row['description'];?></h2>
                     </div>
               <?php } ?>
               <div class="fb-comments"style="font-weight:bold;font-size:1.2em;float:left;margin-left:10%;margin-top:5%;margin-bottom:3%; box-shadow: 0px 14px 32px 0px rgba(0, 0, 0, 0.15);"
