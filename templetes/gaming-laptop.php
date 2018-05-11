5c2a0899f4f25706117f2cdd2d37fdd236670c03
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2018-05-11 14:26:21 +1200

    update information

--- templetes/gaming-laptop.php
+++ templetes/gaming-laptop.php
@@ -19,33 +19,14 @@
 <?php
       include "nav.php";
 ?>
-
-<ul id="skill">
-	<li><span class="bar graphic-design"></span><h3>Graphic Design</h3></li>
-	<li><span class="bar html-css"></span><h3>Html // Css</h3></li>
-	<li><span class="bar jquery"></span><h3>jQuery // MoTools</h3></li>
-	<li><span class="bar wordpress"></span><h3>Wordpress</h3></li>
-</ul>
 <div name="title" style="border-bottom: solid 1px #6998ba;height:100px"><h1 style="color:#FF5077;padding:35px" ><strong>TOP SKILLS</strong></h1></div>
 <section>
-        <?php while ($result && $row = mysqli_fetch_assoc($result)) {?>
-        	<div class="wrapper">
-        				    	<div class="product-img">
-        				      		<img src="<?php echo $row['image'];?>" height="350" width="100%">
-        				    	</div>
-        				    	<div class="product-info">
-        						      <div class="product-text">
-        							        <h1><?php echo $row['title'];?></h1>
-        							        <h2>LAST UPDATED: <?php echo $row['date'];?></h2>
-        							        <p><?php echo $row['description'];?> </p>
-        						      </div>
-        									<div class="product-price-btn">
-        											<button type="button" onclick="location.href='review-detail-gaming.php?id=<?php echo $row['id'];?>';">Read more</button>
-        									</div>
-        				       </div>
-          </div>
-        <?php }
-        				?>
+  <ul id="skill">
+    <li><span class="bar graphic-design"></span><h3>Graphic Design</h3></li>
+    <li><span class="bar html-css"></span><h3>Html // Css</h3></li>
+    <li><span class="bar jquery"></span><h3>jQuery // MoTools</h3></li>
+    <li><span class="bar wordpress"></span><h3>Wordpress</h3></li>
+  </ul>
 </section>
 <?php
       include "footer.php";
