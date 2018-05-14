942d1ad92551d575d761ea1852a862df32928068
nidennisbe <30784398+nidennisbe@users.noreply.github.com>
2018-05-14 23:30:59 +1200

    add google analytics api

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <script async custom-element="amp-analytics"
     src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
 <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy|Russo+One" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700|Open+Sans:300,400,700" rel="stylesheet">
     <link rel="icon" href="resources/pics/logo.png">
     <link rel="stylesheet" type="text/css" href="resources/css/stylesheet.css"/>
     <link rel="stylesheet" type="text/css" href="resources/css/text_effect.css"/>
@@ -41,9 +42,9 @@
                    <div class="box" >
 
                       <h1 class="title_goal">HELLO, I'M NIDENNIS</h1>
-                         <a style="background:url('./resources/pics/classy-bg.jpg');width:100%;margin-bottom:50px" href="https://www.linkedin.com/in/nidennis-be-529572102/">
+                         <div class="cover-image"style="width:100%;margin-bottom:50px" href="https://www.linkedin.com/in/nidennis-be-529572102/">
                            <img class="profile-image" src="./resources/pics/profile1.jpeg"style="width:250px;height:auto"></a>
-                           <h3 class="title_goal">Mobile Developer &nbsp| &nbsp UI Designer</h3>
+                           <h3 class="title_goal">Android Developer &nbsp &nbsp UI Designer</h3>
                          <ul class="social-icons">
                                 <li> <a href="tel:022-025-8124"><img style="width: 35px; height:auto" src="./resources/pics/icon-phone.png" /></a></li>
                                 <li><a href="https://www.facebook.com/nidennisbe" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
@@ -57,11 +58,11 @@
                    <div class="box" >
                      <h1 class="title_goal">Summary</h1>
                     <p id="text-summary"style="text-align: justify">
-I have a few years of experience in building native Android, desktop and Web applications.
-Beside that I really love designing UI for mobile applications as my hobby.
- I enjoy working with technologies as have a strong appreciation for its ability to provide solutions to people and businesses.
- I can’t wait to learn the new technologies that come out everyday in order to deliver the best applications
- that can enhance people’s daily life activities in New Zealand.
+                      I have a few years of experience in building native Android, desktop and Web applications.
+                      Beside that I really love designing UI for mobile applications as my hobby.
+                      I enjoy working with technologies as have a strong appreciation for its ability to provide solutions to people and businesses.
+                      I can’t wait to learn the new technologies that come out everyday in order to deliver the best applications
+                      that can enhance people’s daily life activities in New Zealand.
                     </p>
                     <h1 class="title_goal">Core Skills</h1>
 
